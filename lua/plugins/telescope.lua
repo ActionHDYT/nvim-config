d7d5e09dfd8e2fa4f59737bbf7042415667dc8c1
Fabian Seitz <fabianseitz1412@gmail.com>
2026-07-30 23:46:50 +0200

    added <C-l> to split vertical from telescope select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,38 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		config = function()
-			local telescope = require("telescope")
-			telescope.setup({
-				extensions = {
-					fzf = {
-						fuzzy = true,
-						case_mode = "smart_case",
-					},
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			telescope.load_extension("ui-select")
-			local builtin = require("telescope.builtin")
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-l>"] = actions.select_vertical,
+            }
+          }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            case_mode = "smart_case",
+          },
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      telescope.load_extension("ui-select")
+      local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
-			vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Live grep" })
-			vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Find in help" })
-			vim.keymap.set("n", "gr", builtin.lsp_references, {})
-		end,
-	},
+      vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Find in help" })
+      vim.keymap.set("n", "gr", builtin.lsp_references, {})
+    end,
+  },
 }
